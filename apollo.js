912ed8d4665bf6f0cc7edc7b212b6d98beb8759b
jimin yun <ge72946@gmail.com>
2024-03-07 20:36:26 +0900

    infinite scrolling

--- apollo.js
+++ apollo.js
@@ -40,7 +40,14 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          seeFeed: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
 });
 export default client;
