78dff001c6dde7be5e309a0b5a47ab536dd1b08d
jimin yun <ge72946@gmail.com>
2024-04-04 20:32:10 +0900

    room screen part1

--- navigators/MessagesNav.js
+++ navigators/MessagesNav.js
@@ -10,17 +10,24 @@ export default function MessagesNav() {
   return (
     <Stack.Navigator
       screenOptions={{
+        borderBottomColor: "rgba(255, 255, 255, 0.3)",
+        shadowColor: "rgba(255, 255, 255, 0.3)",
         headerTintColor: "white",
         headerBackTitleVisible: false,
         headerStyle: {
           backgroundColor: "black",
         },
-        headerBackImage: ({ tintColor }) => (
-          <Ionicons color={tintColor} name="chevron-down" size={28} />
-        ),
       }}
     >
-      <Stack.Screen name="Rooms" component={Rooms} />
+      <Stack.Screen
+        name="Rooms"
+        options={{
+          headerBackImage: ({ tintColor }) => (
+            <Ionicons color={tintColor} name="chevron-down" size={30} />
+          ),
+        }}
+        component={Rooms}
+      />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
   );
